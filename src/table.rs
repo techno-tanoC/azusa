7a5703b029b0f7f33bcb0596292fce24952a335d
techno-tanoC <develop.tanoc@gmail.com>
2020-03-20 21:09:06 +0900

    update table

--- src/table.rs
+++ src/table.rs
@@ -1,5 +1,4 @@
 use indexmap::IndexMap;
-use std::hash::Hash;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -8,16 +7,16 @@ use super::item::Item;
 
 pub struct Table<K, V>(Arc<Mutex<IndexMap<K, V>>>);
 
-impl<K: Hash + Eq, V> Table<K, V> {
+impl<V> Table<String, V> {
     pub fn new() -> Self {
         Table(Arc::new(Mutex::new(IndexMap::new())))
     }
 
-    pub async fn add(&self, id: K, v: V) {
-        self.0.lock().await.insert(id, v);
+    pub async fn add(&self, id: impl ToString, value: V) {
+        self.0.lock().await.insert(id.to_string(), value);
     }
 
-    pub async fn delete(&self, id: impl AsRef<K>) {
+    pub async fn delete(&self, id: impl AsRef<str>) {
         self.0.lock().await.shift_remove(id.as_ref());
     }
 }
@@ -29,11 +28,17 @@ impl<T> Table<String, Progress<T>> {
         }
     }
 
-    pub async fn to_vec(&self) -> Vec<Item> {
+    pub async fn to_items(&self) -> Vec<Item> {
         let mut vec = vec![];
         for (k, v) in self.0.lock().await.iter() {
             vec.push(v.to_item(k.clone()).await);
         }
         vec
+    }
+}
+
+impl<K, V> std::clone::Clone for Table<K, V> {
+    fn clone(&self) -> Self {
+        Table(self.0.clone())
     }
 }
