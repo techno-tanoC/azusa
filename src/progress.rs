ca7e74653c369abc1b2a1433e872111e474aa329
techno-tanoC <develop.tanoc@gmail.com>
2020-05-17 20:57:05 +0900

    implement AsyncRead and AsyncSeek to ProgressWriter

--- src/progress.rs
+++ src/progress.rs
@@ -1,9 +1,10 @@
 use serde::Serialize;
+use std::io::SeekFrom;
 use std::pin::Pin;
 use std::sync::atomic::{AtomicBool, AtomicU64, Ordering};
 use std::sync::Arc;
 use std::task::{Poll, Context};
-use tokio::io::{Result, ErrorKind};
+use tokio::io::{AsyncSeek, Result, ErrorKind};
 use tokio::prelude::*;
 
 use crate::item::Item;
@@ -59,6 +60,16 @@ impl<T> ProgressWriter<T> {
     }
 }
 
+impl<T: AsyncRead + Unpin + Send> AsyncRead for ProgressWriter<T> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context,
+        buf: &mut [u8]
+    ) -> Poll<Result<usize>> {
+        Pin::new(&mut self.buf).poll_read(cx, buf)
+    }
+}
+
 impl<T: AsyncWrite + Unpin + Send> AsyncWrite for ProgressWriter<T> {
     fn poll_write(
         mut self: Pin<&mut Self>,
@@ -91,10 +102,28 @@ impl<T: AsyncWrite + Unpin + Send> AsyncWrite for ProgressWriter<T> {
     }
 }
 
+impl<T: AsyncSeek + Unpin + Send> AsyncSeek for ProgressWriter<T> {
+    fn start_seek(
+        self: Pin<&mut Self>,
+        cx: &mut Context,
+        position: SeekFrom
+    ) -> Poll<Result<()>> {
+        Pin::new(&mut self.buf).start_seek(cx, position)
+    }
+
+    fn poll_complete(
+        self: Pin<&mut Self>,
+        cx: &mut Context
+    ) -> Poll<Result<u64>> {
+        Pin::new(&mut self.buf).poll_complete(cx)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use tokio::io::AsyncReadExt;
     use std::io::Cursor;
 
     #[test]
@@ -137,11 +166,36 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn async_read_test() {
+        let mut pg = ProgressWriter::new(Progress::new("name"), Cursor::new(vec![0, 1, 2]));
+        let mut buf = vec![];
+        let n = pg.read_to_end(&mut buf).await.unwrap();
+        assert_eq!(n , 3);
+        assert_eq!(buf, vec![0, 1, 2]);
+    }
+
+    #[tokio::test]
     async fn async_write_test() {
         let mut writer = ProgressWriter::new(Progress::new("name"), Cursor::new(vec![]));
         let buf = [0, 1, 2];
         writer.write_all(&buf).await.unwrap();
         assert_eq!(writer.buf.get_ref(), &vec![0, 1, 2]);
+    }
+
+    #[tokio::test]
+    async fn async_seek_test() {
+        let mut pg = ProgressWriter::new(Progress::new("name"), Cursor::new(vec![0, 1, 2]));
+        let mut buf = vec![];
+
+        let n = pg.read_to_end(&mut buf).await.unwrap();
+        assert_eq!(n , 3);
+        assert_eq!(buf, vec![0, 1, 2]);
+
+        pg.seek(SeekFrom::Start(0)).await.unwrap();
+
+        let n = pg.read_to_end(&mut buf).await.unwrap();
+        assert_eq!(n , 3);
+        assert_eq!(buf, vec![0, 1, 2, 0, 1, 2]);
     }
 
     #[tokio::test]
