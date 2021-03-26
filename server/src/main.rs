e27893d25d12b5d3ff3a3bd8aadbfff0135f0d38
techno-tanoC <develop.tanoc@gmail.com>
2021-03-26 21:31:48 +0900

    server CORS

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,38 @@ use anyhow::Result;
 use rocket_contrib::json::{Json, JsonValue, json};
 use serde::Deserialize;
 
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, ContentType, Method, Status};
+use std::io::Cursor;
+
+pub struct CORS();
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, DELETE, OPTIONS"));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+
+        if request.method() == Method::Options {
+            response.set_status(Status::Ok);
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(0, Cursor::new(""));
+        }
+    }
+}
+
 #[get("/")]
 async fn index() -> JsonValue {
     json!({
@@ -67,6 +99,7 @@ async fn cancel(id: String) -> JsonValue {
 #[tokio::main]
 async fn main() -> Result<()> {
     rocket::ignite()
+        .attach(CORS())
         .mount("/downloads", routes![index, start, cancel])
         .launch()
         .await?;
