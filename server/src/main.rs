13078be4edb518c9753f752aa7d3f9b2c52e0acf
techno-tanoC <develop.tanoc@gmail.com>
2021-03-26 19:55:28 +0900

    mock server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,75 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use anyhow::Result;
+use rocket_contrib::json::{Json, JsonValue, json};
+use serde::Deserialize;
+
+#[get("/")]
+async fn index() -> JsonValue {
+    json!({
+        "status": "ok",
+        "downloads": [
+            {
+                "id": "1",
+                "url": "http://example.com",
+                "name": "example",
+                "total": 1000,
+                "size": 100
+            },
+            {
+                "id": "2",
+                "url": "http://example.com",
+                "name": "example",
+                "total": 1000,
+                "size": 200
+            },
+            {
+                "id": "3",
+                "url": "http://example.com",
+                "name": "example",
+                "total": 1000,
+                "size": 300
+            },
+        ]
+    })
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Start {
+    url: String,
+    name: String,
+    ext: String,
+}
+
+#[post("/", format = "json", data = "<params>")]
+async fn start(params: Json<Start>) -> JsonValue {
+    dbg!(params);
+
+    json!({
+        "status": "ok"
+    })
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct Cancel {
+    id: String,
+}
+
+#[delete("/<id>")]
+async fn cancel(id: String) -> JsonValue {
+    dbg!(id);
+
+    json!({
+        "status": "ok"
+    })
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    rocket::ignite()
+        .mount("/downloads", routes![index, start, cancel])
+        .launch()
+        .await?;
+
+    Ok(())
 }
