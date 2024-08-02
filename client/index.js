bec1662dd5f212778e97413648bbae495f078228
techno-tanoC <develop.tanoc@gmail.com>
2024-08-02 21:16:26 +0900

    Add client

---
+++ client/index.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const fetchItem = async () => {
+  const url = "/downloads"
+  const res = await fetch(url)
+  const json = await res.json()
+  return json
+}
+
+const deleteItem = async (id) => {
+  const url = `/downloads/${id}`
+  await fetch(url, { method: "DELETE" })
+}
+
+const Item = ({id, title, total, size}) => {
+  const percent = `${Math.floor(size * 100 / total)}%`
+  const ratio = `${size.toLocaleString()} / ${total.toLocaleString()}`
+  return (
+    <div className="card">
+      <div className="body">
+        <span style={ { width: percent } } className="progress" />
+        <div className="name">
+          { title }
+        </div>
+        <div className="counts">
+          { percent }
+          <br />
+          { ratio }
+        </div>
+      </div>
+      <button className="button" onClick={ () => deleteItem(id) }>
+        cancel
+      </button>
+    </div>
+  )
+}
+
+const ItemList = ({items}) => {
+  return (
+    <div className="list">
+      {
+        items.map(item => (
+          <Item key={ item.id } id={ item.id } title={ item.title } total={ item.total } size={ item.size } />
+        ))
+      }
+    </div>
+  )
+}
+
+const Page = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const f = async () => {
+      const is = await fetchItem()
+      setItems(is)
+    }
+
+    f()
+    // const intervalId = setInterval(f, 1000)
+    // return () => clearInterval(intervalId)
+  }, [])
+
+  return (
+    <ItemList items={ items } />
+  )
+}
+
+const container = document.querySelector("#root")
+const root = createRoot(container)
+root.render(<Page />)
